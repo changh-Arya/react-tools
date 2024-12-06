35244f7ceee74d9fa6a752e6e88fd0cfbd9a5725
changh-Arya <51989289+changh-Arya@users.noreply.github.com>
2024-12-06 17:00:29 +0800

    Create mock.ts

---
+++ src/views/comparison/mock.ts
@@ -0,0 +1,86 @@
+export const data = [
+  {
+    id: 1,
+    name: 'ManuPremier Protector (MPP)',
+    list: [
+      {
+        title: 'Plan description',
+        expend: false,
+        desc: '2nd product content: ManuLeisure Deferred Annuity is designed to give you a stable stream of '
+      },
+      {
+        title: 'Key features',
+        expend: false,
+        desc: '2nd product content '
+      },
+      {
+        title: 'Guarantees',
+        expend: false,
+        desc: 'Guarantees Guarantees Guarantees '
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'ManuLeisure Defferred Annuity (ManuLeisure)',
+    list: [
+      {
+        title: 'Plan description',
+        expend: false,
+        desc: '2nd product content: ManuLeisure Deferred Annuity '
+      },
+      {
+        title: 'Key features',
+        expend: false,
+        desc: '2nd product content: ManuLeisure Deferred Annuity '
+      },
+      {
+        title: 'Guarantees',
+        expend: false,
+        desc: 'Guarantees Guarantees Guarantees '
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'ManuLeisure Defferred Annuity (ManuLeisure)',
+    list: [
+      {
+        title: 'Plan description',
+        expend: false,
+        desc: '2nd product content: ManuLeisure Deferred Annuity '
+      },
+      {
+        title: 'Key features',
+        expend: false,
+        desc: '2nd product content: ManuLeisure Deferred Annuity '
+      },
+      {
+        title: 'Guarantees',
+        expend: false,
+        desc: 'Guarantees Guarantees222 Guarantees '
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Disability Income ',
+    list: [
+      {
+        title: 'Plan description',
+        expend: false,
+        desc: '2nd product content: ManuLeisure Deferred Annuity is designed to give you a stable stream of retirement annuity income in the future as well as tax benefits now while yo'
+      },
+      {
+        title: 'Key features',
+        expend: false,
+        desc: 'ManuLeisure Deferred Annuity '
+      },
+      {
+        title: 'Guarantees',
+        expend: false,
+        desc: 'Guarantees Guarantees333 Guarantees '
+      }
+    ]
+  }
+]
