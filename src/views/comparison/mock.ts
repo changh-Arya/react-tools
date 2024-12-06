ad7add72bdaef7a77920b1c9c435d789bd75c431
changh-Arya <51989289+changh-Arya@users.noreply.github.com>
2024-12-06 19:04:51 +0800

    Update mock.ts

--- src/views/comparison/mock.ts
+++ src/views/comparison/mock.ts
@@ -1,86 +1,66 @@
-export const data = [
+export const List = [
   {
-    id: 1,
-    name: 'ManuPremier Protector (MPP)',
-    list: [
-      {
-        title: 'Plan description',
-        expend: false,
-        desc: '2nd product content: ManuLeisure Deferred Annuity is designed to give you a stable stream of '
-      },
-      {
-        title: 'Key features',
-        expend: false,
-        desc: '2nd product content '
-      },
-      {
-        title: 'Guarantees',
-        expend: false,
-        desc: 'Guarantees Guarantees Guarantees '
-      }
-    ]
+    path: '/test-demo1',
+    name: 'mainmian Protector (MPP)',
+    guaranteesEn: 'Guarantees',
+    protectionLevelEn: '收到了开发斯库拉多夫看见山东分公司的风格看了几阿斯顿看风景啊还是地方阿贾克斯的发挥空间啊上的联发科里睡觉地方好啦卡上短发 ',
+    issuedAgeEn: ' 测试测试啊上短发哈卡上短发啊剪短发，上短发开了个合适的快乐风格 content.'
   },
   {
-    id: 2,
-    name: 'ManuLeisure Defferred Annuity (ManuLeisure)',
-    list: [
-      {
-        title: 'Plan description',
-        expend: false,
-        desc: '2nd product content: ManuLeisure Deferred Annuity '
-      },
-      {
-        title: 'Key features',
-        expend: false,
-        desc: '2nd product content: ManuLeisure Deferred Annuity '
-      },
-      {
-        title: 'Guarantees',
-        expend: false,
-        desc: 'Guarantees Guarantees Guarantees '
-      }
-    ]
+    path: '/test-demo2',
+    name: 'mainmian Defferred Annuity (mainmian)',
+    guaranteesEn: 'Guarantees Guarantees Guarantees ',
+    protectionLevelEn: ' 上短发空格上短发就看过书打开肌肤 ',
+    issuedAgeEn: '2nd product content: 测试测试啊上短发哈卡上短发啊剪短发，上短发开了个合适的快乐风格 .'
   },
   {
-    id: 2,
-    name: 'ManuLeisure Defferred Annuity (ManuLeisure)',
-    list: [
-      {
-        title: 'Plan description',
-        expend: false,
-        desc: '2nd product content: ManuLeisure Deferred Annuity '
-      },
-      {
-        title: 'Key features',
-        expend: false,
-        desc: '2nd product content: ManuLeisure Deferred Annuity '
-      },
-      {
-        title: 'Guarantees',
-        expend: false,
-        desc: 'Guarantees Guarantees222 Guarantees '
-      }
-    ]
+    path: '/test-demo2',
+    name: 'mainmian Defferred Annuity (mainmian)',
+    guaranteesEn: 'mainmian mainmian mainmian ',
+    protectionLevelEn: ' 上短发空格上短发就看过书打开肌肤 ',
+    issuedAgeEn: '2nd product content: ManuLeis测试测试啊上短发哈卡上短发啊剪短发，上短发开了个合适的快乐风格content.'
+
   },
   {
-    id: 3,
+    path: '/test-demo3',
     name: 'Disability Income ',
-    list: [
-      {
-        title: 'Plan description',
-        expend: false,
-        desc: '2nd product content: ManuLeisure Deferred Annuity is designed to give you a stable stream of retirement annuity income in the future as well as tax benefits now while yo'
-      },
-      {
-        title: 'Key features',
-        expend: false,
-        desc: 'ManuLeisure Deferred Annuity '
-      },
-      {
-        title: 'Guarantees',
-        expend: false,
-        desc: 'Guarantees Guarantees333 Guarantees '
-      }
-    ]
+    guaranteesEn: 'mainmian mainmian mainmian ',
+    protectionLevelEn: ' 上短发空格上短发就看过书打开肌肤 ',
+    issuedAgeEn: '2nd product conten测试测试啊上短发哈卡上短发啊剪短发，上短发开了个合适的快乐风格t:  content.'
+
+  },
+  {
+    path: '/test-demo3',
+    name: 'Disability Income ',
+    guaranteesEn: 'mainmian mainmian mainmian  ',
+    protectionLevelEn: '上短发空格上短发就看过书打开肌肤 ',
+    issuedAgeEn: 'm of re测试测试啊上短发哈卡上短发啊剪短发，上短发开了个合适的快乐风格ow.'
+
+  },
+  {
+    path: '/test-demo4',
+    name: 'test Defferred Annuity (11)',
+    guaranteesEn: 'mainmian mainmian mainmian  ',
+    protectionLevelEn: ' 上短发空格上短发就看过书打开肌肤 ',
+    issuedAgeEn: '2nd prstream of re测试测试啊上短发哈卡上短发啊剪短发，上短发开了个合适的快乐风格 t.'
+
+  }
+]
+
+export const TypeList = [
+  {
+    key: 'guaranteesEn',
+    title: 'Plan description',
+    expend: true
+  },
+  {
+    key: 'protectionLevelEn',
+    title: 'Key features',
+    expend: true
+  },
+  {
+    key: 'issuedAgeEn',
+    title: 'Key features-3',
+    expend: false
   }
 ]
