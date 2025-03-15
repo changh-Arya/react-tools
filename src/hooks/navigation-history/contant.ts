a5129acd799d8aef419fe00ddaeecc3c86fbcbe9
changh-Arya <51989289+changh-Arya@users.noreply.github.com>
2025-03-15 14:00:53 +0800

    Create contant.ts

---
+++ src/hooks/navigation-history/contant.ts
@@ -0,0 +1,7 @@
+import type { MessageDescriptor } from 'react-intl'
+import { messages as productQuickLinkMessages } from '@/view/product-quick-link/message'
+
+export const routeNames: Record<string, MessageDescriptor> = {
+  'products/product-quick-link': productQuickLinkMessages.title
+
+}
